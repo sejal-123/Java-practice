36668e6ee0326ed03f0a9624276cd8325b9c2751
sejal-123 <sejalbudhani@gmail.com>
2025-02-16 23:24:08 +0530

    practice2

--- P2.java
+++
@@ -1,9 +0,0 @@
-class P2 {
-public static void main(String args[]) {
-int r1 = Integer.parseInt(args[0]);
-int r2 = Integer.parseInt(args[1]);
-int res = r1 + r2;
-System.out.println("Sum is " + res);
-
-}
-}
